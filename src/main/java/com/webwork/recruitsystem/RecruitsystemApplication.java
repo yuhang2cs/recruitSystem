ae3a091644022cc6721cd6d6b9006710a395c65c
yuhang2cs <yuhang2cs@gmail.com>
2020-11-08 17:02:28 +0800

    initial project structure

---
+++ src/main/java/com/webwork/recruitsystem/RecruitsystemApplication.java
@@ -0,0 +1,13 @@
+package com.webwork.recruitsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webwork/recruitsystem/RecruitsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webwork.recruitsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
