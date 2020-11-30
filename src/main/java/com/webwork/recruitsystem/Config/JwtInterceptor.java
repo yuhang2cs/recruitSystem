d2f2792a45f8314a0b67d1210d8edb5d78b4b501
yuhang2cs <yuhang2cs@gmail.com>
2020-11-30 13:56:51 +0800

    token生成

---
+++ src/main/java/com/webwork/recruitsystem/Config/JwtInterceptor.java
@@ -0,0 +1,46 @@
+package com.webwork.recruitsystem.Config;
+
+import com.webwork.recruitsystem.utils.TokenUtils;
+import io.jsonwebtoken.Claims;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class JwtInterceptor extends HandlerInterceptorAdapter {
+    @Autowired
+    private Audience audience;
+    private TokenUtils JwtTokenUtil;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 获取请求头信息authorization信息
+        final String authHeader = request.getHeader(JwtTokenUtil.AUTH_HEADER_KEY);
+        if(authHeader==null){
+            return false;
+        }
+        log.info("## authHeader= {}", authHeader);
+        if (StringUtils.isBlank(authHeader) || !authHeader.startsWith(JwtTokenUtil.TOKEN_PREFIX)) {
+            log.info("### 用户未登录，请先登录 ###");
+        }
+        // 获取token
+        final String token = authHeader.substring(7);
+        if(audience == null){
+            BeanFactory factory = WebApplicationContextUtils.getRequiredWebApplicationContext(request.getServletContext());
+            audience = (Audience) factory.getBean("audience");
+        }
+        // 验证token是否有效--无效已做异常抛出，由全局异常处理后返回对应信息
+        Claims claims = JwtTokenUtil.parseJWT(token, audience.getBase64Secret());
+        if(claims!=null){
+            return true;
+        }
+        return false;
+    }
+}
